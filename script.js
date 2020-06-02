9a71139d01586718893f1c8b8aca7f0f3ca5216a
esmail mahbub <esml.mahbub>
2020-06-01 22:59:31 -0400

    update

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ var quizQuestions = [{
 // Other global variables
 var finalQuestionIndex = quizQuestions.length;
 var currentQuestionIndex = 0;
-var timeLeft = 76;
+var timeLeft = 60;
 var timerInterval;
 var score = 0;
 var correct;
@@ -180,7 +180,7 @@ function replayQuiz(){
     highscoreContainer.style.display = "none";
     gameoverDiv.style.display = "none";
     startQuizDiv.style.display = "flex";
-    timeLeft = 76;
+    timeLeft = 60;
     score = 0;
     currentQuestionIndex = 0;
 }
